63445019b3a986093eeaaf11f5c8ec67c5b0915c
Michael Paulini <mh6@sanger.ac.uk>
2023-06-20 16:23:13 +0100

    fixed some typos

--- src/lib/CurationTool.cr
+++ src/lib/CurationTool.cr
@@ -11,9 +11,9 @@ module CurationTool
     Dir.mkdir_p(wd)
 
     fasta_gz = y.decon_file.sub("contamination", "decontaminated.fa.gz")
-    fasta_gz = "zcat " + fasta_gz    
+    fasta_gz = "zcat " + fasta_gz
 
-    #if a merged file exists
+    # if a merged file exists
     merged_fasta = "/lustre/scratch123/tol/teams/grit/geval_pipeline/geval_runs/*/#{y.sample_version}/raw/merged.fa" # needs a glob[0]
     fasta_gz = "cat " + merged_fasta if File.exists?(merged_fasta)
 
@@ -30,11 +30,11 @@ HERE
     raise "something went wrong" unless $?.success?
   end
 
-  #shorthand to build a pretext
-  def build_pretext(y,fasta,out,highres = false)
-	cmd = "/software/grit/projects/vgp_curation_scripts/Pretext_HiC_pipeline.sh -i #{fasta} -s #{out} -k #{y.hic_read_dir} -d `pwd` #{highres_option}"
-	puts `#{cmd}`
-	raise "something went wrong" unless $?.success?
+  # shorthand to build a pretext
+  def build_pretext(y, fasta, out_file, highres = false)
+    cmd = "/software/grit/projects/vgp_curation_scripts/Pretext_HiC_pipeline.sh -i #{fasta} -s #{out_file} -k #{y.hic_read_dir} -d `pwd` #{highres}"
+    puts `#{cmd}`
+    raise "something went wrong" unless $?.success?
   end
 
   # make files from the preetxt agp and build a new pretext
@@ -58,22 +58,22 @@ HERE
       if File.exists?("HAP1.tpf")
         puts `split_hap_tpf.rb rapid_prtxt.tpf`
         raise "something went wrong" unless $?.success?
- 
-        1.upto(4){|i|
+
+        1.upto(4) { |i|
           next unless File.exists?("HAP#{i}_shrapnel.tpf")
-          cmd=<<-HERE
+          cmd = <<-HERE
           cat HAP#{i}_shrapnel.tpf >> HAP#{i}.tpf`;
           rapid_join.pl -csv HAP#{i}.csv -fa original.fa -tpf HAP#{i}.tpf -out #{id}_HAP#{i}
           HERE
           puts `#{cmd}`
           raise "something went wrong" unless $?.success?
-       
-          build_pretext(y,id+"_HAP1.curated_primary.no_mt.unscrubbed.fa",id+"_HAP1",highres_option)
+
+          build_pretext(y, id + "_HAP1.curated_primary.no_mt.unscrubbed.fa", id + "_HAP1", highres_option)
         }
-        2.upto(4){|i|
+        2.upto(4) { |i|
           next unless File.exists?("#{id}_HAP#{i}.curated_primary.no_mt.unscrubbed.fa")
           # might need bsubbing, which could be bsub -K -M 8000 -R'select[mem>8000] rusage[mem=8000]'
-          cmd=<<-HERE
+          cmd = <<-HERE
           hap2_hap1_ID_mapping.sh #{id}_HAP1.curated_primary.no_mt.unscrubbed.fa #{id}_HAP#{i}.curated_primary.no_mt.unscrubbed.fa ;
           mv hap2_hap1.tsv hap#{i}_hap1.tsv ;
           update_mapping.rb -f #{id}_HAP#{i}.curated_primary.no_mt.unscrubbed.fa -t hap#{i}_hap1.tsv -c #{id}_HAP#{i}.updated_chromosome.list -n hap#{i}.remapping > #{id}_HAP#{i}.curated_primary.no_mt.unscrubbed.updated.fa ;
@@ -81,19 +81,18 @@ HERE
           HERE
           puts `#{cmd}`
           raise "something went wrong" unless $?.success?
-          build_pretext(y,id+"_HAP"+i+".curated_primary.no_mt.unscrubbed.fa",id+"_HAP"+i,highres_option)
+          build_pretext(y, "#{id}_HAP#{i}.curated_primary.no_mt.unscrubbed.fa", "#{id}_HAP#{i}", highres_option)
         }
-
       else
-	cmd = <<-HERE
+        cmd = <<-HERE
 	[ -s haps_rapid_prtxt_XL.tpf ] && rapid_join.pl -fa original.fa -tpf haps_rapid_prtxt_XL.tpf -out #{id} -hap ;
 	rapid_join.pl -csv chrs.csv -fa original.fa -tpf rapid_prtxt_XL.tpf -out #{id} ;
 	HERE
-	puts `$cmd`
-	raise "something went wrong" unless $?.success?
-	
-	# Make new pretext map.
-	cmd = "/software/grit/projects/vgp_curation_scripts/Pretext_HiC_pipeline.sh -i #{id}.curated_primary.no_mt.unscrubbed.fa -s #{id} -k #{y.hic_read_dir} -d `pwd` #{highres_option}"
+        puts `$cmd`
+        raise "something went wrong" unless $?.success?
+
+        # Make new pretext map.
+        cmd = "/software/grit/projects/vgp_curation_scripts/Pretext_HiC_pipeline.sh -i #{id}.curated_primary.no_mt.unscrubbed.fa -s #{id} -k #{y.hic_read_dir} -d `pwd` #{highres_option}"
         build_pretext(y, id + "curated_primary.no_mt.unscxrubbed.fa", id, highres_option)
       end
 
