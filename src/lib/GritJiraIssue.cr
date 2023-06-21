21a0d46ef833ff4a68c2ad66b3e52f5ad6fb980c
Michael Paulini <mh6@sanger.ac.uk>
2023-06-21 09:51:14 +0100

    scientific->any for the fallback

--- src/lib/GritJiraIssue.cr
+++ src/lib/GritJiraIssue.cr
@@ -123,7 +123,7 @@ class GritJiraIssue
     json = JSON.parse(r.body)
 
     if json.as_a.size < 1
-      r = HTTP::Client.get("https://www.ebi.ac.uk/ena/taxonomy/rest/scientific-name/#{common_name}", headers: HTTP::Headers{"Accept" => "application/json"})
+      r = HTTP::Client.get("https://www.ebi.ac.uk/ena/taxonomy/rest/any-name/#{common_name}", headers: HTTP::Headers{"Accept" => "application/json"})
       raise "cannot get the taxonomy" unless r.success?
 
       json = JSON.parse(r.body)
