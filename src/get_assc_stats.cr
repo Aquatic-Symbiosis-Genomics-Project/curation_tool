e2be80d36c077509684fcca35b83bf52039f7199
Michael Paulini <mh6@sanger.ac.uk>
2023-08-23 14:36:52 +0100

    added a small thing to calculate ascc stats

---
+++ src/get_assc_stats.cr
@@ -0,0 +1,34 @@
+#!/bin/env crystal
+
+require "./lib/GritJiraIssue"
+
+def parse_decon_file(f)
+  i = [] of String
+  File.each_line(f) do |line|
+    if /^REMOVE\s+(\S+)/i.match(line)
+      i << $1
+    end
+  end
+  i
+end
+
+def parse_bed_file(f)
+  i = [] of String
+  File.each_line(f) do |line|
+    if /^(\S+)\s.*REMOVE/i.match(line)
+      i << $1
+    end
+  end
+  i
+end
+
+ARGV.each { |jira_id|
+  y = GritJiraIssue.new(jira_id)
+  contamination_ids = parse_decon_file(y.decon_file)
+
+  bed_file = "{y.decon_file}.bed"
+  next unless File.exists?(bed_file)
+  bed_ids = parse_bed_file(bed_file)
+
+  puts [y.tol_id, contamination_ids & bed_ids, puts contamination_ids - bed_ids, bed_ids - contamination_ids].join("\t")
+}
