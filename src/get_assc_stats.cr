85a441c5e5b013336f8ef4cfd2a52d337b77c539
Michael Paulini <mh6@sanger.ac.uk>
2023-08-23 15:03:10 +0100

    fix

--- src/get_assc_stats.cr
+++ src/get_assc_stats.cr
@@ -26,13 +26,9 @@ ARGV.each { |jira_id|
   y = GritJiraIssue.new(jira_id)
 
   contamination_ids = parse_decon_file(y.decon_file)
-  puts ".con #{contamination_ids}"
-
-  bed_file = "{y.decon_file}.bed"
-  puts bed_file
+  bed_file = "#{y.decon_file}.bed"
   next unless File.exists?(bed_file)
   bed_ids = parse_bed_file(bed_file)
-  puts ".bed #{bed_ids}"
 
   puts [y.tol_id, contamination_ids & bed_ids, puts contamination_ids - bed_ids, bed_ids - contamination_ids].join("\t")
 }
