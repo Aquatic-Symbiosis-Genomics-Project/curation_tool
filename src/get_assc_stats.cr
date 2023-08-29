20520aa9d2ba9908a461be38b74da34386c9bd47
Michael Paulini <mh6@sanger.ac.uk>
2023-08-29 17:14:03 +0100

    added dustmasker to the collection (which means it needs to run on the farm)

--- src/get_assc_stats.cr
+++ src/get_assc_stats.cr
@@ -35,13 +35,15 @@ end
 def length_and_gc(f)
   l = Hash(String, Int32).new
   g = Hash(String, Float64).new
+  r = Hash(String, Float64).new
   fp = GzipReader.new(f)
   fx = FastxReader.new(fp)
   fx.each { |e|
     l[e.name] = e.seq.size
     g[e.name] = e.seq.count("gcGC")/e.seq.size
+    r[e.name] = e.seq.count("[a-z]")/e.seq.size
   }
-  return(l, g)
+  return(l, g, r)
 end
 
 def av(l : Array(Int32 | Float64))
@@ -61,7 +63,10 @@ def get_ave(h, l : Array(String))
   av(l.map { |k| h[k] })
 end
 
-puts ["tolID", "fasta file", "average gc", "average length", "true positives", "average gc tp", "average len tp", "false positives", "average gc fp", "average len fp", "false negatives", "average gc fn", "average len fn"].join("\t")
+puts ["tolID", "fasta file", "average gc", "average length", "average repeat",
+      "true positives", "average gc tp", "average len tp", "average repeat tp",
+      "false positives", "average gc fp", "average len fp", "average repeat fp",
+      "false negatives", "average gc fn", "average len fn", "average repeat fn"].join("\t")
 
 ARGV.each { |jira_id|
   y = StatIssue.new(jira_id)
@@ -72,9 +77,16 @@ ARGV.each { |jira_id|
     next unless File.exists?(bed_file)
     bed_ids = parse_bed_file(bed_file)
 
-    fasta_file = c.gsub(".contamination", ".fa.gz")
-    next unless File.exists?(fasta_file)
-    ln, gc = length_and_gc(fasta_file)
+    fasta_file = c.gsub(".contamination", ".fa")
+    next unless File.exists?(fasta_file + ".gz")
+
+    masked_file = fasta_file + ".masked.gz"
+    unless File.exists?(masked_file)
+      `zcat #{fasta_file}.gz | /software/grit/bin/ncbi-blast-2.10.0+/bin/dustmasker -outfmt fasta | gzip -9 -c #{masked_file}`
+      raise "couldn't create #{masked_file}" unless $?.success?
+    end
+
+    ln, gc, rep = length_and_gc(masked_file)
     true_positives = contamination_ids & bed_ids
     false_positives = bed_ids - contamination_ids
     false_negatives = contamination_ids - bed_ids
@@ -85,6 +97,7 @@ ARGV.each { |jira_id|
       columns << s.size
       columns << get_ave(gc, s)
       columns << get_ave(ln, s)
+      columns << get_ave(rep, s)
     }
     puts columns.join("\t")
   }
