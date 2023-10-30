7b345d716b6da74e4fc3712e64467a3e25639250
Michael Paulini <mh6@sanger.ac.uk>
2023-10-30 14:14:14 +0000

    fixed the useless asignment

--- src/lib/CurationTool.cr
+++ src/lib/CurationTool.cr
@@ -11,7 +11,7 @@ module CurationTool
     Dir.mkdir_p(wd)
 
     fasta_gz = y.decon_file.sub("contamination", "decontaminated.fa.gz")
-    fasta_gz = y.decon_file.sub("contamination.bed", "decontaminated.fa.gz")
+    fasta_gz = fasta_gz.sub("contamination.bed", "decontaminated.fa.gz")
 
     raise Exception.new("scaffolds.tpf in working #{wd} already exists") if File.exists?(wd + "/scaffolds.tpf")
 
