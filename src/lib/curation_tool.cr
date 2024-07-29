3da2bb6a88253925c1fd55d1012de40d195e9121
Michael Paulini <mh6@sanger.ac.uk>
2024-07-29 16:39:18 +0100

    removed the default group

--- src/lib/curation_tool.cr
+++ src/lib/curation_tool.cr
@@ -44,7 +44,7 @@ HERE
       puts `#{cmd}`
       raise "something went wrong" unless $?.success?
 
-      bsub = "bsub -G grit-grp -K -M 16G -R'select[mem>16G] rusage[mem=16G]' -o /dev/null"
+      bsub = "bsub -K -M 16G -R'select[mem>16G] rusage[mem=16G]' -o /dev/null"
       # create fasta
       if y.merged
         ["HAP1", "HAP2"].each { |label|
