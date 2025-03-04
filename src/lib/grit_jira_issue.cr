5c0aa8bb4eda0e9a98cb3946dd7da22e114330f5
Michael Paulini <mh6@sanger.ac.uk>
2025-03-04 10:14:22 +0000

    try the auto cleanup

--- src/lib/grit_jira_issue.cr
+++ src/lib/grit_jira_issue.cr
@@ -159,7 +159,8 @@ curationpretext.sh -profile sanger,singularity --input #{Path[fasta].expand} \
 --longread #{self.pacbio_read_dir}/fasta \
 --outdir #{output} \
 --map_order length \
--resume #{telo}
+#{telo} \
+-cleanup
 
 HERE
   end
