ace9fc2c52ebe5e33c08cf09d20caaefa34ad2b8
Michael Paulini <mh6@sanger.ac.uk>
2025-03-04 16:09:51 +0000

    change the time formatting

--- src/lib/curation_tool.cr
+++ src/lib/curation_tool.cr
@@ -54,7 +54,7 @@ module CurationTool
             raise "something went wrong with #{cmd}" unless $?.success?
           }
           # Make new pretext map for hap1.
-          cmd = y.curation_pretext("#{id}.hap1.fa", "#{id}.hap1.curationpretext.#{Time.utc}")
+          cmd = y.curation_pretext("#{id}.hap1.fa", "#{id}.hap1.curationpretext.#{Time.utc.to_s("%Y-%m-%d_%H:%M:%S")}")
           puts `#{cmd}`
           raise "something went wrong" unless $?.success?
         else
@@ -63,7 +63,7 @@ module CurationTool
           raise "something went wrong with #{cmd}" unless $?.success?
           # Make new pretext map.
           cmd = "Pretext_HiC_pipeline.sh -i #{id}.fa -s #{id} -d .  -k #{y.hic_read_dir} &"
-          cmd = y.curation_pretext("#{id}.fa", "#{id}.curationpretext.#{Time.utc}")
+          cmd = y.curation_pretext("#{id}.fa", "#{id}.curationpretext.#{Time.utc.to_s("%Y-%m-%d_%H:%M:%S")}")
           puts `#{cmd}`
           raise "something went wrong" unless $?.success?
         end
